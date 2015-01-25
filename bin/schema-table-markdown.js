bde9331b67c9ab15967fb56f853e59cb07376339
Raynos <raynos2@gmail.com>
2015-01-24 23:01:47 -0800

    implementation of schema-table-markdown

---
+++ bin/schema-table-markdown.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var jsonBody = require('body/json');
+var parseArgs = require('minimist');
+var process = require('process');
+var console = require('console');
+
+var toMarkdown = require('../index.js');
+
+module.exports = main;
+
+if (require.main === module) {
+    main(parseArgs(process.argv.slice(2)));
+}
+
+function main() {
+    jsonBody(process.stdin, onBody);
+
+    function onBody(err, schemaTable) {
+        if (err) {
+            throw err;
+        }
+
+        var markdown = toMarkdown(schemaTable);
+        process.stdout.write(markdown);
+    }
+}
--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 'use strict';
 
+var parseJSONSchema = require('json-schema-to-jsig');
+var serializeJSIG = require('jsig/serialize');
+
 module.exports = schemaTableMarkdown;
 
-function schemaTableMarkdown() {
+function schemaTableMarkdown(schemaTable) {
+    var ops = Object.keys(schemaTable);
+    var markdown = '# Schema documentation\n\n';
 
+    for (var i = 0; i < ops.length; i++) {
+        var op = ops[i];
+
+        markdown += '## ' + op + '\n\n';
+
+        var spec = schemaTable[op];
+        var requestJSIG = toJSIGStr(spec.request);
+        var responseJSIG = toJSIGStr(spec.response);
+
+        markdown += '### Request spec\n\n';
+
+        markdown += '```ocaml\n' + requestJSIG + '\n```\n\n';
+
+        markdown += '### Response spec\n\n';
+
+        markdown += '```ocaml\n' + responseJSIG + '\n```\n\n';
+    }
+
+    return markdown;
+}
+
+function toJSIGStr(schema) {
+    return serializeJSIG(parseJSONSchema(schema));
 }
